1443ac3513551b6ebebb52d293c4fba230ced5c8
Aryan Sinha <techyminati@users.noreply.github.com>
2020-11-26 17:59:28 +0530

    The TG Bot : Mod Bringup

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import os
 from git import Repo
 from shutil import copytree, rmtree
-GITHUB_REPO = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot").split()
+GITHUB_REPO = os.environ.get("GITHUB_REPO_LINK", "https://github.com/techyminati/The-TG-Bot").split()
 GITHUB_REPO_LINK = GITHUB_REPO[0]
 try:
   GITHUB_REPO_BRANCH = GITHUB_REPO[1]
 except:
   GITHUB_REPO_BRANCH = "v3"
-print("Downloading latest version of The-TG-Bot..")
+print("Downloading latest version of The-TG-Bot Mod by TechyMinati..")
 Repo.clone_from(GITHUB_REPO_LINK, ".ota", branch=GITHUB_REPO_BRANCH)
 copytree(".ota", ".", dirs_exist_ok=True)
 rmtree(".ota")
