0b1171dd59d78a17b1f65b1277dd45c801e1ce8a
Aryan Sinha <63485082+techyminati@users.noreply.github.com>
2020-12-05 17:38:55 +0530

    New Reverse module rekting bot, So revert back

--- modules/reverse.py
+++ modules/reverse.py
@@ -1,146 +1,81 @@
-# Thanks to Xtra Telegram for this awesome module !!
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot module for reverse searching stickers and images on Google """
+# Taken from Friday userbot
 
-import io
 import os
-import urllib
-from urllib.request import urlopen
+import asyncio
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
-import re
-from telethon.tl.types import MessageMediaPhoto
-from PIL import Image
-
-opener = urllib.request.build_opener()
-useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36'
-opener.addheaders = [('User-agent', useragent)]
+from google_images_download import google_images_download
 
 
-@register(outgoing=True, pattern=r"^.reverse(?: |$)(\d*)")
-@errors_handler
-async def okgoogle(img):
-    """ For .reverse command, Google search images and stickers. """
-    if os.path.isfile("okgoogle.png"):
-        os.remove("okgoogle.png")
+def progress(current, total):
+    logger.info(
+        "Downloaded {} of {}\nCompleted {}".format(
+            current, total, (current / total) * 100
+        )
+    )
 
-    message = await img.get_reply_message()
-    if message and message.media:
-        photo = io.BytesIO()
-        await bot.download_media(message, photo)
-    else:
-        await img.edit("`Reply to photo or sticker nigger.`")
+
+@client.on(events("grs"))
+async def _(event):
+    if event.fwd_from:
         return
-
-    if photo:
-        await img.edit("`Processing...`")
-        try:
-            image = Image.open(photo)
-        except OSError:
-            await img.edit('`Unsupported sexuality, most likely.`')
-            return
-        name = "okgoogle.png"
-        image.save(name, "PNG")
-        image.close()
-        # https://stackoverflow.com/questions/23270175/google-reverse-image-search-using-post-request#28792943
-        searchUrl = 'https://www.google.com/searchbyimage/upload'
-        multipart = {
-            'encoded_image': (name, open(name, 'rb')),
-            'image_content': ''
+    start = datetime.now()
+    BASE_URL = "http://www.google.com"
+    OUTPUT_STR = "`Thats not an image or a sticker you retard`"
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        previous_message_text = previous_message.message
+        if previous_message.media:
+            downloaded_file_name = await client.download_media(
+                previous_message, os.path.join(ENV.DOWNLOAD_DIRECTORY, "reverse_img.png")
+            )
+            SEARCH_URL = "{}/searchbyimage/upload".format(BASE_URL)
+            multipart = {
+                "encoded_image": (
+                    downloaded_file_name,
+                    open(downloaded_file_name, "rb"),
+                ),
+                "image_content": "",
+            }
+            # https://stackoverflow.com/a/28792943/4723940
+            google_rs_response = requests.post(
+                SEARCH_URL, files=multipart, allow_redirects=False
+            )
+            the_location = google_rs_response.headers.get("Location")
+            os.remove(downloaded_file_name)
+        else:
+            previous_message_text = previous_message.message
+            SEARCH_URL = "{}/searchbyimage?image_url={}"
+            request_url = SEARCH_URL.format(BASE_URL, previous_message_text)
+            google_rs_response = requests.get(request_url, allow_redirects=False)
+            the_location = google_rs_response.headers.get("Location")
+        await event.edit("`Calling Sundar Pichai..`")
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
         }
-        response = requests.post(searchUrl,
-                                 files=multipart,
-                                 allow_redirects=False)
-        fetchUrl = response.headers['Location']
-
-        if response != 400:
-            await img.edit("`Image successfully uploaded to Google. Maybe.`"
-                           "\n`Parsing source now. Maybe.`")
-        else:
-            await img.edit("`Google told me to fuck off.`")
-            return
-
-        os.remove(name)
-        match = await ParseSauce(fetchUrl +
-                                 "&preferences?hl=en&fg=1#languages")
-        guess = match['best_guess']
-        imgspage = match['similar_images']
-
-        if guess and imgspage:
-            await img.edit(f"[{guess}]({fetchUrl})\n\n`Looking for this Image...`")
-        else:
-            await img.edit("`Can't find this piece of shit.`")
-            return
-
-        if img.pattern_match.group(1):
-            lim = img.pattern_match.group(1)
-        else:
-            lim = 3
-        images = await scam(match, lim)
-        yeet = []
-        for i in images:
-            k = requests.get(i)
-            yeet.append(k.content)
-        try:
-            await img.client.send_file(entity=await
-                                       img.client.get_input_entity(img.chat_id
-                                                                   ),
-                                       file=yeet,
-                                       reply_to=img)
-        except TypeError:
-            pass
-        await img.edit(
-            f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})")
-
-
-async def ParseSauce(googleurl):
-    """Parse/Scrape the HTML code for the info we want."""
-
-    source = opener.open(googleurl).read()
-    soup = BeautifulSoup(source, 'html.parser')
-
-    results = {'similar_images': '', 'best_guess': ''}
-
-    try:
-        for similar_image in soup.findAll('input', {'class': 'gLFyf'}):
-            url = 'https://www.google.com/search?tbm=isch&q=' + \
-                urllib.parse.quote_plus(similar_image.get('value'))
-            results['similar_images'] = url
-    except BaseException:
-        pass
-
-    for best_guess in soup.findAll('div', attrs={'class': 'r5a77d'}):
-        results['best_guess'] = best_guess.get_text()
-
-    return results
-
-
-async def scam(results, lim):
-
-    single = opener.open(results['similar_images']).read()
-    decoded = single.decode('utf-8')
-
-    imglinks = []
-    counter = 0
-
-    pattern = r'^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$'
-    oboi = re.findall(pattern, decoded, re.I | re.M)
-
-    for imglink in oboi:
-        counter += 1
-        if not counter >= int(lim):
-            imglinks.append(imglink)
-        else:
-            break
-
-    return imglinks
-
-
-CMD_HELP.update({
-    'reverse':
-    '.reverse\
-        \nUsage: Reply to a pic/sticker to revers-search it on Google Images !!'
+        response = requests.get(the_location, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+        # document.getElementsByClassName("r5a77d"): PRS
+        prs_div = soup.find_all("div", {"class": "r5a77d"})[0]
+        prs_anchor_element = prs_div.find("a")
+        prs_url = BASE_URL + prs_anchor_element.get("href")
+        prs_text = prs_anchor_element.text
+        # document.getElementById("jHnbRc")
+        img_size_div = soup.find(id="jHnbRc")
+        img_size = img_size_div.find_all("div")
+        end = datetime.now()
+        ms = (end - start).seconds
+        OUTPUT_STR = """{img_size}
+<b>Possible Related Search</b>: <a href="{prs_url}">{prs_text}</a>
+<b>More Info</b>: Open this <a href="{the_location}">Link</a>
+<b>Time Taken</b>: {ms} seconds""".format(
+            **locals()
+        )
+    await event.edit(OUTPUT_STR, parse_mode="HTML")
+ENV.HELPER.update({
+    "reverse": "\
+```.grs <in reply to image/sticker/gif>```\
+\nUsage: Reverse Searches the image on google.\
+"
 })
